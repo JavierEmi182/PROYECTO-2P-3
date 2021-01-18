a76feaf1090731b29a7281d488a38a3a6a547fd5
PaulGudino <pgudino@espol.edu.ec>
2021-01-18 00:21:04 -0500

    Desarrollo de mesero

--- src/main/java/com/pooespol/apprestaurant/IniciarMeseroController.java
+++ src/main/java/com/pooespol/apprestaurant/IniciarMeseroController.java
@@ -5,15 +5,19 @@
  */
 package com.pooespol.apprestaurant;
 
+import com.pooespol.apprestaurant.modelo.Mesa;
 import com.pooespol.apprestaurant.modelo.login.Mesero;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 /**
  * FXML Controller class
@@ -25,8 +29,10 @@ public class IniciarMeseroController implements Initializable {
 
     @FXML
     private Label lbMeseroNombre;
-    @FXML
-    private Circle fg1;
+
+    private Circle circulo;
+    private Mesero mesero;
+    private Label nmesa;
     /**
      * Initializes the controller class.
      */
@@ -36,7 +42,7 @@ public class IniciarMeseroController implements Initializable {
         // nombre del mesero al lbl lblMesero.setText()
         
         if (LoginController.usuario1 instanceof Mesero){
-            Mesero mesero = (Mesero)LoginController.usuario1;
+            mesero = (Mesero)LoginController.usuario1;
             lbMeseroNombre.setText("Mesero "+ mesero.getNombre() );
         }
         
@@ -44,9 +50,17 @@ public class IniciarMeseroController implements Initializable {
 
     @FXML
     private void disponibilidad(MouseEvent event) throws IOException {
-       
-            App.setRoot("TomaPedido");
-        
+         App.setRoot("TomaPedido");
+         circulo = (Circle) ((Pane)event.getSource()).getChildren().get(0);
+         if (circulo.getFill() == Color.YELLOW){
+             nmesa = (Label)((Pane)event.getSource()).getChildren().get(1);
+             int x = Integer.parseInt(nmesa.getText());
+             Mesa mesaocup = new Mesa(0, x, mesero);
+             
+             circulo.setFill(Color.GREEN);
+             ((Pane)event.getSource()).getChildren().set(0, circulo);
+         }
+         
     }
     
 }
